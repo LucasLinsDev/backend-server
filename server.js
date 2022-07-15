8d8d92375beb6ececb7baf469c863b130dea1586
Lucas Lins <lucaslinsperfil@gmail.com>
2022-07-15 13:38:31 -0300

    criação do método que paga as informações do endpoint

--- server.js
+++ server.js
@@ -41,9 +41,16 @@ const url='https://travellog.myvtex.com/api/oms/pvt/orders?q=1246930501700-01';
     console.log(error);
   }
 })
+
+
+
 app.post('/api/v1/pontos',async(req,res)=>{
- 
-  })
+
+  const {OrderId}= req.body;
+  console.log(OrderId);
+  return res.json({message:OrderId})
+
+})
   
 app.listen(port,()=>{
   console.log(`SERVER APP LISTENING ON PORT ${port}`)
